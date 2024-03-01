6ab13db6ac93a9bf45e3d9a6df76bb34a2f018e6
Vittor <dev.vittor@gmail.com>
2024-03-01 01:29:09 -0400

    fix: status bar padding wrong

--- lib/src/slider.dart
+++ lib/src/slider.dart
@@ -175,6 +175,11 @@ class SliderDrawerState extends State<SliderDrawer>
         color: _appBarColor,
         child: Column(
           children: <Widget>[
+            // Remove space status bar
+            SizedBox(
+              height: MediaQuery.of(context).viewPadding.top,
+              width: double.infinity,
+            ),
             if (widget.appBar != null && widget.appBar is SliderAppBar)
               SAppBar(
                 isCupertino: widget.isCupertino,
